ea1076c46170e8bccab4db0daaa7e09c5dfa9a00
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-08-04 20:05:33 +0600

    Add files via upload

---
+++ free_ice_cream.cpp
@@ -0,0 +1,59 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+// #include <string>
+// #include <sstream>
+// #include<set>
+// #include<iomanip>
+// #include<cstring>
+// #include<cctype> //islower () issuper () for check the first char
+// #include<algorithm>
+// #include<cmath>
+#define sortarray sort(arr,arr+x);
+// #define rev reverse(s1.begin(), s1.end());
+// #define uppertolowers1 transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
+// #define uppertolowers2 transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
+// #define descsort sort(arr, arr + n, greater<int>());
+#define sp " "
+#define forn(i, n) for (int i = 0; i < (n); i++)
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define maxelementofvector *max_element(digit.begin(),digit.end());
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+void solve ()
+{
+    ll x,sum;cin>>x>>sum;
+    ll cnt=0;
+    vector<pair<char,int>>vv(x);
+   for (int i=0;i<x;i++)
+   {
+        char temp;cin>>temp;
+        ll temp1;cin>>temp1;
+        vv[i].first=temp;
+        vv[i].second=temp1;
+   }
+    for (int i=0;i<x;i++) 
+    {
+        if (vv[i].first=='+')sum+=vv[i].second;
+        else
+        {
+            if (sum>=vv[i].second)
+            {
+                sum-=vv[i].second;
+            }else cnt++;
+        }
+    }
+    cout<<sum<<sp<<cnt<<endl;
+}
+int main ()
+{
+    speed;
+    // int tt;
+    // cin>>tt;
+    // while (tt--)
+    // {
+        solve ();
+    // }
+    return 0;
+}
