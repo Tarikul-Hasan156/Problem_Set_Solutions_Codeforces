d922f64ddef86bbf38ed604bba71a896246681a7
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-08-11 00:01:25 +0600

    Add files via upload

---
+++ lever.cpp
@@ -0,0 +1,47 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+// #include <string>
+// #include <sstream>
+// #include<set>
+// #include<iomanip>
+// #include<cstring>
+// #include<cctype> //islower () issuper () for check the first char
+// #include<algorithm>
+// #include<cmath>
+#define sortarray sort(arr,arr+x);
+// #define rev reverse(s1.begin(), s1.end());
+// #define uppertolowers1 transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
+// #define uppertolowers2 transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
+// #define descsort sort(arr, arr + n, greater<int>());
+#define sp " "
+#define forn(i, n) for (int i = 0; i < (n); i++)
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define maxelementofvector *max_element(digit.begin(),digit.end());
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+void solve ()
+{
+    ll x;cin>>x;
+    vector<int>temp1(x),temp2(x);
+    for ( int i=0;i<x;i++)cin>>temp1[i];
+    for ( int i=0;i<x;i++)cin>>temp2[i];
+    ll res=1;
+    for (int i=0;i<x;i++)
+    {
+        if (temp1[i]>temp2[i])res+=temp1[i]-temp2[i];
+    }
+    cout<<res<<endl;
+}
+int main ()
+{
+    speed;
+    int tt;
+    cin>>tt;
+    while (tt--)
+    {
+        solve ();
+    }
+    return 0;
+}
