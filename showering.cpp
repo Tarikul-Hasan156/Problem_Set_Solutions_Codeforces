95bc06851116daee3b045919d2f394de38f3f65e
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-08-13 22:15:47 +0600

    Add files via upload

---
+++ showering.cpp
@@ -0,0 +1,49 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+// #include <string>
+// #include <sstream>
+// #include<set>
+// #include<iomanip>
+// #include<cstring>
+// #include<cctype> //islower () issuper () for check the first char
+// #include<algorithm>
+// #include<cmath>
+#define sortarray sort(arr,arr+x);
+// #define rev reverse(s1.begin(), s1.end());
+// #define uppertolowers1 transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
+// #define uppertolowers2 transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
+// #define descsort sort(arr, arr + n, greater<int>());
+#define sp " "
+#define forn(i, n) for (int i = 0; i < (n); i++)
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define maxelementofvector *max_element(digit.begin(),digit.end());
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+void solve ()
+{
+     ll n,s,m;cin>>n>>s>>m;
+     ll l=0;
+     vector<pair<int,int>>vv(n);
+     for (auto &x:vv)cin>>x.first>>x.second;
+     sort(vv.begin(),vv.end());
+    if (vv[0].first>=s){cout<<"YES"<<endl;return;}
+    for (int i=1;i<n;i++)
+    {
+        if (vv[i].first-vv[i-1].second>=s){cout<<"YES"<<endl;return;}
+    }
+    if (m-vv[n-1].second>=s)cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+int main ()
+{
+    speed;
+    int tt;
+    cin>>tt;
+    while (tt--)
+    {
+        solve ();
+    }
+    return 0;
+}
