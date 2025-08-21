fe72e92d739bbd2508d5d3105ced43d07faf720a
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-08-21 23:21:19 +0600

    Add files via upload

---
+++ Home_work.cpp
@@ -0,0 +1,56 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+// #include <string>
+// #include <sstream>
+// #include<set>
+// #include<iomanip>
+// #include<cstring>
+// #include<cctype> //islower () issuper () for check the first char
+// #include<algorithm>
+// #include<cmath>
+#define sortarray sort(arr,arr+x);
+// #define rev reverse(s1.begin(), s1.end());
+// #define uppertolowers1 transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
+// #define uppertolowers2 transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
+// #define descsort sort(arr, arr + n, greater<int>());
+#define sp " "
+#define forn(i, n) for (int i = 0; i < (n); i++)
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define maxelementofvector *max_element(digit.begin(),digit.end());
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+void solve ()
+{
+    ll x; cin>>x;
+    string s1;cin>>s1;
+    ll y;cin>>y;
+    string s;cin>>s;
+    string temp;cin>>temp;
+    vector<char>ch;
+    vector<char>ch2;
+    for (int i=0;i<y;i++)
+    {
+        if (temp[i]=='V')ch.push_back(s[i]);
+        else ch2.push_back(s[i]);
+    }
+    reverse(ch.begin(),ch.end());
+    for (auto &c:ch)cout<<c;
+    cout<<s1;
+    
+    for (auto &c:ch2)cout<<c;
+    cout<<endl;
+
+}
+int main ()
+{
+    speed;
+    int tt;
+    cin>>tt;
+    while (tt--)
+    {
+        solve ();
+    }
+    return 0;
+}
