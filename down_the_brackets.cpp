52147dad1734ce8818f6fdf5aa47bc1ca774cfbc
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-08-24 06:33:50 +0600

    Add files via upload

---
+++ down_the_brackets.cpp
@@ -0,0 +1,53 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+// #include <string>
+// #include <sstream>
+// #include<set>
+// #include<iomanip>
+// #include<cstring>
+// #include<cctype> //islower () issuper () for check the first char
+// #include<algorithm>
+// #include<cmath>
+#define sortarray sort(arr,arr+x);
+// #define rev reverse(s1.begin(), s1.end());
+// #define uppertolowers1 transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
+// #define uppertolowers2 transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
+// #define descsort sort(arr, arr + n, greater<int>());
+#define sp " "
+#define forn(i, n) for (int i = 0; i < (n); i++)
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define maxelementofvector *max_element(digit.begin(),digit.end());
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+void solve ()
+{
+    string s;cin>>s;
+    ll res=0,cnt=0,temp1=0,temp2=0;
+    for (int i=0;i<s.size();i++)
+    {
+        if (s[i]=='(')temp1++;
+        else if (s[i]==')')
+        {
+            temp2++;
+            if (temp1==temp2)
+            {
+                cnt++;
+                temp1=0,temp2=0;
+            }
+        }
+    }
+    cout<<((cnt>1)?"YES":"NO")<<endl;
+}
+int main ()
+{
+    speed;
+    int tt;
+    cin>>tt;
+    while (tt--)
+    {
+        solve ();
+    }
+    return 0;
+}
