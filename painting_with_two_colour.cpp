1efa533add57fa79a93c3d46cfec0f849e584e7c
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-08-27 00:12:54 +0600

    Add files via upload

---
+++ painting_with_two_colour.cpp
@@ -0,0 +1,56 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+// #include <string>
+// #include <sstream>
+// #include<set>
+// #include<iomanip>
+// #include<cstring>
+// #include<cctype> //islower () issuper () for check the first char
+// #include<algorithm>
+// #include<cmath>
+#define sortarray sort(arr,arr+x);
+// #define rev reverse(s1.begin(), s1.end());
+// #define uppertolowers1 transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
+// #define uppertolowers2 transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
+// #define descsort sort(arr, arr + n, greater<int>());
+#define sp " "
+#define forn(i, n) for (int i = 0; i < (n); i++)
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define maxelementofvector *max_element(digit.begin(),digit.end());
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+void solve ()
+{
+    ll n,a,b;cin>>n>>a>>b;
+    if ((n%2==0 && b%2==0) || (n&1 && b&1))
+    {
+        if (b&1)
+        {
+            if (a%2==0)
+            {
+                cout<<((a<=b)?"YES":"NO")<<endl;
+            }else cout<<"YES"<<endl;
+        } else
+        {
+            if (a&1)
+            {
+                cout<<((a<=b)?"YES":"NO")<<endl;
+            }else cout<<"YES"<<endl;
+        }
+    }
+    else
+    cout<<"NO"<<endl;
+}
+int main ()
+{   
+    speed;
+    int tt;
+    cin>>tt;
+    while (tt--)
+    {
+        solve ();
+    }
+    return 0;
+}
