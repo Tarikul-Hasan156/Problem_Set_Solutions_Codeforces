8b242c99335d3de043fd507423b5dc4e3a21d9fa
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-10-01 16:09:32 +0600

    Add files via upload

---
+++ buying_shovels.cpp
@@ -0,0 +1,53 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+// #include <string>
+// #include <sstream>
+// #include<set>
+// #include<iomanip>
+// #include<cstring>
+// #include<cctype> //islower () issuper () for check the first char
+// #include<algorithm>
+// #include<cmath>
+#define sortarray sort(arr,arr+x);
+// #define rev reverse(s1.begin(), s1.end());
+// #define uppertolowers1 transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
+// #define uppertolowers2 transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
+// #define descsort sort(arr, arr + n, greater<int>());
+#define sp " "
+#define forn(i, n) for (int i = 0; i < (n); i++)
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define maxelementofvector *max_element(digit.begin(),digit.end());
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+using namespace std;
+void solve ()
+{
+    ll x,y;cin>>x>>y;
+    if (y>=x){cout<<1<<endl;return;}
+    set<ll>st;
+    for (int i=1;i*i<=x;i++)
+    {
+        if (x%i==0)
+        {
+            st.insert(i);
+            st.insert(x/i);
+        }
+    }
+    vector<ll>vv(st.begin(),st.end());
+    for (int i=vv.size()-1;i>=0;i--)
+    {
+        if (vv[i]<=y){cout<<x/vv[i]<<endl;return;}
+    }
+}
+int main ()
+{
+    speed;
+    int tt;
+    cin>>tt;
+    while (tt--)
+    {
+        solve ();
+    }
+    return 0;
+}
