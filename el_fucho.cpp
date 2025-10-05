b53f0c07717d657acd4b652748fa4a76e3a1de86
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-10-06 00:59:13 +0600

    Add files via upload

---
+++ el_fucho.cpp
@@ -0,0 +1,25 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define sp " "
+#define endl '\n'
+using namespace std;
+     
+void solve ()
+{
+    ll x;cin>>x;
+    ll ans=0;
+    cout<<(x-1)+(x-2)+1<<endl;
+}
+int main ()
+{
+    speed;
+    int tt;
+    cin>>tt;
+    while (tt--)
+    {
+        solve ();
+    }
+    return 0;
+}
