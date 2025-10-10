eafe3e89f4c3fc031e942b3eb83b9feb70ac7378
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-10-10 22:55:03 +0600

    Add files via upload

---
+++ circle_of_apple_trees.cpp
@@ -0,0 +1,32 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define sp " "
+#define endl '\n'
+using namespace std;
+     
+void solve ()
+{
+    ll x;cin>>x;
+    vector<ll>vv(x);
+    for (auto&x:vv)cin>>x;
+    ll cnt=1;
+    sort(vv.begin(),vv.end());
+    for (int i=0;i<x-1;i++)
+    {
+        if (vv[i+1]>vv[i])cnt++;
+    }
+    cout<<cnt<<endl;
+}
+int main ()
+{
+    speed;
+    int tt;
+    cin>>tt;
+    while (tt--)
+    {
+        solve ();
+    }
+    return 0;
+}
