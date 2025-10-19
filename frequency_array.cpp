9e1dd53ca3cc46cc39016b5c9222a0fe90c4d7b2
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-10-19 16:34:56 +0600

    Add files via upload

---
+++ frequency_array.cpp
@@ -0,0 +1,31 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define sp " "
+#define endl '\n'
+using namespace std;
+     
+void solve ()
+{
+    ll x,m;cin>>x>>m;
+    vector<ll>vv(x),hash(m+1,0);
+    for (int i=0;i<x;i++){
+        cin>>vv[i];
+        hash[vv[i]]++;
+    }
+    for (int i=1;i<=m;i++){
+        cout<<hash[i]<<endl;
+    }
+}
+int main ()
+{
+    speed;
+    // int tt;
+    // cin>>tt;
+    // while (tt--)
+    // {
+        solve ();
+    // }
+    return 0;
+}
