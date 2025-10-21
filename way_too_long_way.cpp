d6594ad56dfab515296df9feb5e521301635eadb
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-10-21 22:24:55 +0600

    Add files via upload

---
+++ way_too_long_way.cpp
@@ -0,0 +1,25 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define sp " "
+#define endl '\n'
+using namespace std;
+     
+void solve ()
+{
+    string s;cin>>s;
+    if (s.size()<11){cout<<s<<endl;return;}
+    cout<<s[0]<<s.size()-2<<s[s.size()-1]<<endl;
+}
+int main ()
+{
+    speed;
+    int tt;
+    cin>>tt;
+    while (tt--)
+    {
+        solve ();
+    }
+    return 0;
+}
