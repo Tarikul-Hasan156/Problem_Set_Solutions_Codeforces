c8a4fa51402750289a60537a6043a0f9ce9557d1
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-10-21 21:47:44 +0600

    Add files via upload

---
+++ create_a_new_string.cpp
@@ -0,0 +1,25 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define sp " "
+#define endl '\n'
+using namespace std;
+     
+void solve ()
+{
+    string s1,s2;cin>>s1>>s2;
+    cout<<s1.size()<<sp<<s2.size()<<endl;
+    cout<<s1<<sp<<s2<<endl;
+}
+int main ()
+{
+    speed;
+    // int tt;
+    // cin>>tt;
+    // while (tt--)
+    // {
+        solve ();
+    // }
+    return 0;
+}
