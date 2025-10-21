75b454b554b08325a707827932170cd23552fcda
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-10-21 21:55:24 +0600

    Add files via upload

---
+++ let's_use_getline.cpp
@@ -0,0 +1,29 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define sp " "
+#define endl '\n'
+using namespace std;
+     
+void solve ()
+{
+    string s;
+    getline(cin,s);
+    for (int i=0;i<s.size();i++){
+        if (s[i]=='\\')break;
+        cout<<s[i];
+    }
+    cout<<endl;
+}
+int main ()
+{
+    speed;
+    // int tt;
+    // cin>>tt;
+    // while (tt--)
+    // {
+        solve ();
+    // }
+    return 0;
+}
