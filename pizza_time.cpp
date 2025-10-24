41c153431177f8435b6014f76188dc4a82c71dd6
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-10-24 23:45:13 +0600

    Add files via upload

---
+++ pizza_time.cpp
@@ -0,0 +1,31 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define sp " "
+#define endl '\n'
+using namespace std;
+     
+void solve ()
+{
+    ll x;cin>>x;
+    ll temp=x;
+    ll ans=0;
+    while (temp>=3){
+        ll temp2=temp/3;
+        ans+=temp2;
+        temp=temp-(2*temp2);
+    }
+    cout<<ans<<endl;
+}
+int main ()
+{
+    speed;
+    int tt;
+    cin>>tt;
+    while (tt--)
+    {
+        solve ();
+    }
+    return 0;
+}
