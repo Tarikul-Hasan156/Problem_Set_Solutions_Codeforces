7c6613b34e3dc74f89930563f720ac06ea82453f
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-10-28 23:21:18 +0600

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,25 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define sp " "
+#define endl '\n'
+using namespace std;
+     
+void solve ()
+{
+    ll a,b,c,d;cin>>a>>b>>c>>d;
+    if (a==b and b==c and c==d)cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+int main ()
+{
+    speed;
+    int tt;
+    cin>>tt;
+    while (tt--)
+    {
+        solve ();
+    }
+    return 0;
+}
