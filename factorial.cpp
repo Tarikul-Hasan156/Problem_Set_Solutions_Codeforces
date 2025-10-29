995f63c4154bcf754478e31fa9ffe5c8bda22d8f
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-10-30 02:06:18 +0600

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,28 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define sp " "
+#define endl '\n'
+using namespace std;
+ ll fact(ll x){
+    if (x==0)return 1;
+    return x*fact(x-1);
+ }
+void solve ()
+{
+    ll x;cin>>x;
+   ll res= fact(x);
+   cout<<res<<endl;
+}
+int main ()
+{
+    speed;
+    // int tt;
+    // cin>>tt;
+    // while (tt--)
+    // {
+        solve ();
+    // }
+    return 0;
+}
