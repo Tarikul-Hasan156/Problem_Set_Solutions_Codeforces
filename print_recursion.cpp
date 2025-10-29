79d0e37d2852a1d8809b6544727fb97a77ad8fdd
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-10-30 00:22:50 +0600

    Add files via upload

---
+++ print_recursion.cpp
@@ -0,0 +1,29 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define sp " "
+#define endl '\n'
+using namespace std;
+ void  print (ll x){
+    if (x==0)return;
+    cout<<"I love Recursion"<<endl;
+     print(--x);
+     
+ }
+void solve ()
+{
+    ll x;cin>>x;
+    print(x);
+}
+int main ()
+{
+    speed;
+    // int tt;
+    // cin>>tt;
+    // while (tt--)
+    // {
+        solve ();
+    // }
+    return 0;
+}
