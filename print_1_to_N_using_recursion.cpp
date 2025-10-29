266bb3ba896e56ef1f454306c824d87095c62a30
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-10-30 00:34:54 +0600

    Add files via upload

---
+++ print_1_to_N_using_recursion.cpp
@@ -0,0 +1,28 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define sp " "
+#define endl '\n'
+using namespace std;
+void print(ll x){
+    if (x==1)return;
+    print(--x);
+    cout<<x<<endl;
+}
+void solve ()
+{
+    ll x;cin>>x;
+    print(x+1);
+}
+int main ()
+{
+    speed;
+    // int tt;
+    // cin>>tt;
+    // while (tt--)
+    // {
+        solve ();
+    // }
+    return 0;
+}
