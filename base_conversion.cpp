23a774ac77690a1d711da436af350364d60d6a39
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-10-30 01:28:52 +0600

    Add files via upload

---
+++ base_conversion.cpp
@@ -0,0 +1,28 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define sp " "
+#define endl '\n'
+using namespace std;
+void conversion (ll x,ll i){
+    if (x==0)return;
+    conversion(x/2,i);
+    cout<<((x>>i)&1);
+}
+void solve (){
+    ll x;cin>>x;
+    conversion(x,0);
+    cout<<endl;
+}
+int main ()
+{
+    speed;
+    int tt;
+    cin>>tt;
+    while (tt--)
+    {
+        solve ();
+    }
+    return 0;
+}
