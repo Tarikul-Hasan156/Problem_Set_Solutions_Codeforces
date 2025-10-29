d7a8742f7a82f11d2dcaa644377e9631a405a83d
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-10-30 03:25:09 +0600

    Add files via upload

---
+++ log2.cpp
@@ -0,0 +1,30 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define sp " "
+#define endl '\n'
+using namespace std;
+ ll cnt=0;
+ void log_two(ll x){
+    if (x==1)return;
+    cnt++;
+    log_two(x/2);
+ }
+void solve ()
+{
+    ll x;cin>>x;
+    log_two(x);
+    cout<<cnt<<endl;
+}
+int main ()
+{
+    speed;
+    // int tt;
+    // cin>>tt;
+    // while (tt--)
+    // {
+        solve ();
+    // }
+    return 0;
+}
