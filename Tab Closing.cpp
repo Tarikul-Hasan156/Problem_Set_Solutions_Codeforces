a6316e6466d8b746a509632f29fa2a25c60c37b6
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-11-16 23:24:19 +0600

    Add files via upload

---
+++ Tab Closing.cpp
@@ -0,0 +1,38 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define sp " "
+#define endl '\n'
+using namespace std;
+     
+void solve ()
+{
+    ll a,b,c;cin>>a>>b>>c;
+    if (a==b){
+        cout<<1<<endl;
+        return;
+    }
+    if (c>((double)a/b) ){
+        cout<<2<<endl;
+        return;
+    }
+   ll ans=1;
+   ll temp=(b*c--);
+   while (a<temp){
+        ans++;
+        temp=b*c--;
+   }
+   cout<<ans<<endl;
+}
+int main ()
+{
+    speed;
+    int tt;
+    cin>>tt;
+    while (tt--)
+    {
+        solve ();
+    }
+    return 0;
+}
