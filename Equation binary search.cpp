8a158e508d3c1bfeedf8dd0264f5cab91e7e1a10
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-12-01 07:32:04 +0600

    Add files via upload

---
+++ Equation binary search.cpp
@@ -0,0 +1,51 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define sp ' '
+#define endl '\n'
+using namespace std;
+
+// struct custom_hash {
+//   static uint64_t splitmix64(uint64_t x) {
+//       x += 0x9e3779b97f4a7c15ULL;
+//       x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9ULL;
+//       x = (x ^ (x >> 27)) * 0x94d049bb133111ebULL;
+//       return x ^ (x >> 31);
+//   }
+//
+//   size_t operator()(uint64_t x) const {
+//       static const uint64_t FIXED_RANDOM =
+//           chrono::steady_clock::now().time_since_epoch().count();
+//       return splitmix64(x + FIXED_RANDOM);
+//   }
+// };
+double f(double x){
+    return x*x+sqrt(x);
+}
+void solve ()
+{
+    double c;cin>>c;
+    double l=0,r=1e5;
+    double ans=0;
+    while (r-l>1e-7){
+        double mid=(l+r)/2.0;
+        if (f(mid)>=c){
+            ans=mid;
+           r=mid;
+        }else l=mid;
+    }
+    cout <<fixed<<setprecision(10)<<ans<<endl;
+}
+
+int main ()
+{
+    speed;
+    // int tt;
+    // cin >> tt;
+    // while (tt--)
+    // {
+        solve();
+    // }
+    return 0;
+}
