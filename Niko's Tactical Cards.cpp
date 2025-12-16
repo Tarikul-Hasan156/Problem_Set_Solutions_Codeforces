7e903f6b4a64ee8570881ce35a8d49d404ab4ecd
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-12-16 21:05:40 +0600

    Add files via upload

---
+++ Niko's Tactical Cards.cpp
@@ -0,0 +1,49 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define sp ' '
+#define endl '\n'
+using namespace std;
+
+// struct custom_hash {
+//   static uint64_t splitmix64(uint64_t x) {
+//       x += 0x9e3779b97f4a7c15ULL;
+//       x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9ULL;
+//       x = (x ^ (x >> 27)) * 0x94d049bb133111ebULL;
+//       return x ^ (x >> 31);
+//   }
+//
+//   size_t operator()(uint64_t x) const {
+//       static const uint64_t FIXED_RANDOM =
+//           chrono::steady_clock::now().time_since_epoch().count();
+//       return splitmix64(x + FIXED_RANDOM);
+//   }
+// };
+
+void solve ()
+{
+    ll x;cin>>x;
+    vector<ll>a(x),b(x);
+    for (auto &x:a)cin>>x;
+    for (auto &x:b)cin>>x;
+    ll k1=0,k2=0;
+    for (int i=0;i<x;i++){
+        ll temp1=k1;
+        k1=max(k1-a[i],b[i]-k2);
+        k2=min(k2-a[i],b[i]-temp1);
+    }
+    cout<<k1<<endl;
+}
+
+int main ()
+{
+    speed;
+    int tt;
+    cin >> tt;
+    while (tt--)
+    {
+        solve();
+    }
+    return 0;
+}
