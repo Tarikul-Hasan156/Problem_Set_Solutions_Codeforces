f3f71ec1bcf5070285a89f0c8abc8624b7006972
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-12-29 22:46:02 +0600

    Add files via upload

---
+++ New Year String.cpp
@@ -0,0 +1,53 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define sp ' '
+#define endl '\n'
+using namespace std;
+const ll N=1e5+9;
+
+// struct custom_hash {
+//   static uint64_t splitmix64(uint64_t x) {
+//       x += 0x9e3779b97f4a7c15ULL;
+//       x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9ULL;
+//       x = (x ^ (x >> 27)) * 0x94d049bb133111ebULL;
+//       return x ^ (x >> 31);
+//   }
+//
+//   size_t operator()(uint64_t x) const {
+//       static const uint64_t FIXED_RANDOM =
+//           chrono::steady_clock::now().time_since_epoch().count();
+//       return splitmix64(x + FIXED_RANDOM);
+//   }
+// };
+bool check2(string s){
+    return s.find("2025")!=string::npos;
+}
+bool check1(string s){
+    return s.find("2026")!=string::npos;
+}
+void solve ()
+{
+    int x;cin>>x;
+    string s;cin>>s;
+    if (check1(s) ){
+        cout<<0<<endl;return;
+    }
+    if (check2(s)){
+        cout<<1<<endl;return;
+    }
+    cout<<0<<endl;
+}
+
+int main ()
+{
+    speed;
+    int tt;
+    cin >> tt;
+    while (tt--)
+    {
+        solve();
+    }
+    return 0;
+}
