ab979652cfa96f8f8eeb9193cd05937a842be913
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2025-12-29 22:46:34 +0600

    Add files via upload

---
+++ Production of Snowmen.cpp
@@ -0,0 +1,67 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define sp ' '
+#define endl '\n'
+using namespace std;
+const ll N=1e5+9;
+
+// struct custom_hash {
+//   static uint64_t splitmix64(uint64_t x) {
+//       x += 0x9e3779b97f4a7c15ULL;
+//       x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9ULL;
+//       x = (x ^ (x >> 27)) * 0x94d049bb133111ebULL;
+//       return x ^ (x >> 31);
+//   }
+//
+//   size_t operator()(uint64_t x) const {
+//       static const uint64_t FIXED_RANDOM =
+//           chrono::steady_clock::now().time_since_epoch().count();
+//       return splitmix64(x + FIXED_RANDOM);
+//   }
+// };
+vector<ll>a(N),b(N),c(N);
+void solve ()
+{
+    ll x;cin>>x;
+    for (int i=1;i<=x;i++)cin>>a[i];
+    for (int i=1;i<=x;i++)cin>>b[i];
+    for (int i=1;i<=x;i++)cin>>c[i];
+    ll cnt1=0,cnt2=0;
+    for (int i=1;i<=x;i++){
+        bool flag=true;
+        for (int j=1;j<=x;j++){
+            if (a[j]>=b[(i+j-1)%x+1]){
+                flag=false;
+                break;
+            }
+        }
+        if (flag)cnt1++;
+        // cout<<cnt1<<endl;
+    }
+    for (int i=1;i<=x;i++){
+        bool flag=true;
+        for (int j=1;j<=x;j++){
+            if (b[j]>=c[(i+j-1)%x+1]){
+                flag=false;
+                break;
+            }
+        }
+        if(flag)cnt2++;
+    }
+    cout<<(ll)x*cnt1*cnt2<<endl;
+
+}
+
+int main ()
+{
+    speed;
+    int tt;
+    cin >> tt;
+    while (tt--)
+    {
+        solve();
+    }
+    return 0;
+}
