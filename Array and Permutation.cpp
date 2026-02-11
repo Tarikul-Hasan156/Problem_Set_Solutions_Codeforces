ff592ff7baa76029491c9d784af39f4c39e93d36
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2026-02-12 00:50:41 +0600

    Add files via upload

---
+++ Array and Permutation.cpp
@@ -0,0 +1,58 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define sp ' '
+#define endl '\n'
+using namespace std;
+const ll N=1e5+9;
+
+// struct custom_hash {
+//   static uint64_t splitmix64(uint64_t x) {
+//       x += 0x9e3779b97f4a7c15ULL;
+//       x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9ULL;
+//       x = (x ^ (x >> 27)) * 0x94d049bb133111ebULL;
+//       return x ^ (x >> 31);
+//   }
+//
+//   size_t operator()(uint64_t x) const {
+//       static const uint64_t FIXED_RANDOM =
+//           chrono::steady_clock::now().time_since_epoch().count();
+//       return splitmix64(x + FIXED_RANDOM);
+//   }
+// };
+
+void solve ()
+{
+    int n;cin>>n;
+    vector<int>a(n),b(n),index;
+    map<int,int>mp;
+    int i=0;
+    for (auto &x:a){
+        cin>>x;
+        mp[x]=i;
+        i++;
+    }
+    for (auto &x:b)cin>>x;
+    for (int i=0;i<n;i++){
+        index.push_back(mp[b[i]]);
+    }
+    auto index2=index;
+    auto check=index;
+    sort(index2.rbegin(),index2.rend());
+    if (is_sorted(index.begin(),index.end())){
+        cout<<"YES"<<endl;
+    }else cout<<"NO"<<endl;
+}
+
+int main ()
+{
+    speed;
+    int tt;
+    cin >> tt;
+    while (tt--)
+    {
+        solve();
+    }
+    return 0;
+}
