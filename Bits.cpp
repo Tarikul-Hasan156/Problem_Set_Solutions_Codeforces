94b35a5f9c9095eb48700e2b9f4629ec1acc7163
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2026-02-28 07:26:48 +0600

    Add files via upload

---
+++ Bits.cpp
@@ -0,0 +1,66 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll int64_t
+#define sp ' '
+#define endl '\n'
+using namespace std;
+const ll N=1e5+9;
+
+// struct custom_hash {
+//   static uint64_t splitmix64(uint64_t x) {
+//       x += 0x9e3779b97f4a7c15ULL;
+//       x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9ULL;
+//       x = (x ^ (x >> 27)) * 0x94d049bb133111ebULL;
+//       return x ^ (x >> 31);
+//   }
+//
+//   size_t operator()(uint64_t x) const {
+//       static const uint64_t FIXED_RANDOM =
+//           chrono::steady_clock::now().time_since_epoch().count();
+//       return splitmix64(x + FIXED_RANDOM);
+//   }
+// };
+
+void solve ()
+{
+    ll l, r; cin >> l >> r;
+    if (l==r){
+        cout<<l<<endl;return;
+    }
+    bitset<64>bs_r(r),bs_l(l);
+    ll ans=l;
+    int index=0;
+    for (int i=0;i<64;i++){
+       if (bs_l[i]==1){
+            index=i;
+            break;
+       }
+    }
+    // cout<<bs_l<<endl;
+    for (int i=0;i<64;i++){
+        if (bs_l[i]==1){
+            continue;
+        }else{
+            // cout<<i<<sp;
+            ll temp=(1LL<<i);
+            if (ans+temp<=r){
+                ans+=temp;
+            }else break;
+        }
+    }
+    // cout<<endl;
+    cout<<ans<<endl;
+}
+
+int main ()
+{
+    speed;
+    int tt;
+    cin >> tt;
+    while (tt--)
+    {
+        solve();
+    }
+    return 0;
+}
