ca686cdb83522f300b4d7bc30fa6f1813428058d
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2026-03-14 00:39:07 +0600

    Add files via upload

---
+++ High School Become Human.cpp
@@ -0,0 +1,43 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll long long
+#define sp ' '
+#define endl '\n'
+using namespace std;
+const ll N=1e5+9;
+
+// struct custom_hash {
+//   static uint64_t splitmix64(uint64_t x) {
+//       x += 0x9e3779b97f4a7c15ULL;
+//       x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9ULL;
+//       x = (x ^ (x >> 27)) * 0x94d049bb133111ebULL;
+//       return x ^ (x >> 31);
+//   }
+//
+//   size_t operator()(uint64_t x) const {
+//       static const uint64_t FIXED_RANDOM =
+//           chrono::steady_clock::now().time_since_epoch().count();
+//       return splitmix64(x + FIXED_RANDOM);
+//   }
+// };
+char check (int x , int y){
+    double _x=y*log10(x);
+    double _y=x*log10(y);
+    if (_x>_y)return '>';
+    else if (_x<_y)return '<';
+    else return '=';
+}
+void solve ()
+{
+    int x,y;
+    cin>>x>>y;
+    cout<<check(x,y)<<endl;
+}
+
+int main ()
+{
+    speed;
+    solve();
+    return 0;
+}
