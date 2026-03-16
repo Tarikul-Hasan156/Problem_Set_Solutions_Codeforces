066b2b98e7d7099ab16fdd5f7c357ff130f33f92
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2026-03-17 02:03:07 +0600

    Add files via upload

---
+++ Right Maximum.cpp
@@ -0,0 +1,54 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll long long
+#define sp ' '
+#define endl '\n'
+using namespace std;
+const ll N=1e5+9;
+
+// struct custom_hash {
+//   static uint64_t splitmix64(uint64_t x) {
+//       x += 0x9e3779b97f4a7c15ULL;
+//       x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9ULL;
+//       x = (x ^ (x >> 27)) * 0x94d049bb133111ebULL;
+//       return x ^ (x >> 31);
+//   }
+//
+//   size_t operator()(uint64_t x) const {
+//       static const uint64_t FIXED_RANDOM =
+//           chrono::steady_clock::now().time_since_epoch().count();
+//       return splitmix64(x + FIXED_RANDOM);
+//   }
+// };
+
+void solve ()
+{
+    int n;cin>>n;
+    vector<int>a(n+1);
+    int temp=0;
+    for (int i=1;i<=n;i++){
+        int val;cin>>val;
+        if (val>=temp){
+            temp=val;
+            a[i]=i;
+        }else a[i]=a[i-1];
+    }
+    int ans=0;
+    for (int i=n;i>=1;i--){
+        if (a[i]==i)ans++;
+    }
+    cout<<ans<<endl;
+}
+
+int main ()
+{
+    speed;
+    int tt;
+    cin >> tt;
+    while (tt--)
+    {
+        solve();
+    }
+    return 0;
+}
