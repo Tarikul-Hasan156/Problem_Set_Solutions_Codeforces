f83e1e8035792ebe54442564b389b14a6ceb4135
Md. Tarikul Hasan Dipu <tarikulhasandipu@gmail.com>
2026-06-06 04:28:21 +0600

    Add files via upload

---
+++ GCD - Greatest Clever Dude.cpp
@@ -0,0 +1,25 @@
+//بِسْمِ ٱللّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+#include <bits/stdc++.h>
+#define speed ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll long long
+#define sp ' '
+#define endl '\n'
+using namespace std;
+const ll N=1e5+9;
+
+void solve ()
+{
+    string a, b; cin >> a >> b;
+    if (a == b) {
+         cout << a << endl;
+    }else {
+        cout << 1 << endl;
+    }
+}
+
+int main ()
+{
+    speed;
+    solve();
+    return 0;
+}
